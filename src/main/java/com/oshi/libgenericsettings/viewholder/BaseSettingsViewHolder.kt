9e4d60c3b6ef617b87d6d1d07588332fb4a446f2
udi oshi <udioshi@gmail.com>
2018-02-01 09:58:18 +0200

    + Removed analytics code
    + Upgraded to Kotlin 1.2.21

--- src/main/java/com/oshi/libgenericsettings/viewholder/BaseSettingsViewHolder.kt
+++ src/main/java/com/oshi/libgenericsettings/viewholder/BaseSettingsViewHolder.kt
@@ -2,18 +2,11 @@ package com.oshi.libgenericsettings.viewholder
 
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
-import com.oshi.libgenericsettings.GenericSettingsAnalytics
-import com.oshi.libgenericsettings.GenericSettingsLib
 import com.oshi.libgenericsettings.data.BaseViewTypeData
-import com.oshi.libgenericsettings.helper.ANALYTICS_CATEGORIES
 import com.oshi.libgenericsettings.presenter.ISettingsPresenter
 
 
 abstract class BaseSettingsViewHolder<in T : BaseViewTypeData>(var viewDataBinding : ViewDataBinding) : RecyclerView.ViewHolder(viewDataBinding.root) {
-
-    init {
-        GenericSettingsLib.getGenericSettingsAnalytics(itemView.context).sendEvent(ANALYTICS_CATEGORIES.CATEGORY_VIEW.categoryName, javaClass.simpleName)
-    }
 
     abstract fun onBind(data: T, presenter: ISettingsPresenter, position: Int)
 
